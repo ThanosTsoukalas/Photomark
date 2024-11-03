e65a4bda6c256b9f2425ae256d7906c38c709791
= <=>
2024-11-03 16:37:23 +0200

    Added aero theme to some of the components

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-let isOnMenuDropDown = 0;
-function dropDownInit(dropDownNum){
-    if (dropDownNum == 1){
-        if (document.getElementById('dropdown-content-1').style.display == 'none'){
-            document.getElementById('dropdown-content-1').style.display = 'block';
-        }
-    }
-}
-
